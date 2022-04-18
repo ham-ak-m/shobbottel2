ff5d05411a1bc3fefad6eeb98353e10d0e1e73a3
hamed-ak-m <akbarzadeh.m.hamed@protonmail.com>
2022-04-18 15:51:50 +0430

    fav list added

--- app/bot/middleware/KeyboardMiddleware.js
+++ app/bot/middleware/KeyboardMiddleware.js
@@ -1,13 +1,18 @@
 const {
   MAIN_BUTTON_TEXT,
   categoryListButton,
-  commentTypeButtons
+  commentTypeButtons,
+  productListButton
 } = require("../utils/ButtonManager");
-const { COMMENT_FIRST_MESSAGE } = require('../utils/MessageHandler');
+const {
+  COMMENT_FIRST_MESSAGE,
+  FAV_LIST_MESSAGE,
+  FAV_LIST_EMPTY_MESSAGE
+} = require("../utils/MessageHandler");
 const { CATEGORY_LIST } = require("../utils/MessageHandler");
-const { STATE_LIST } = require('./SessionMiddleware');
-const Category = require('../../model/category');
-
+const { STATE_LIST } = require("./SessionMiddleware");
+const Category = require("../../model/category");
+const User = require("../../model/user");
 
 module.exports = (ctx, next) => {
   if (!ctx.message) return next();
@@ -19,26 +24,30 @@ module.exports = (ctx, next) => {
 };
 
 const EventListener = {
-  [MAIN_BUTTON_TEXT.ONLINE_BUY]: async (ctx) => {
-    const categoryList = await Category.find()
+  [MAIN_BUTTON_TEXT.ONLINE_BUY]: async ctx => {
+    const categoryList = await Category.find();
     ctx.reply(CATEGORY_LIST, categoryListButton(categoryList));
   },
-  [MAIN_BUTTON_TEXT.COMMENT]: (ctx) => {
-    ctx.session.state = STATE_LIST.COMMENT_TYPE
+  [MAIN_BUTTON_TEXT.COMMENT]: ctx => {
+    ctx.session.state = STATE_LIST.COMMENT_TYPE;
     ctx.reply(COMMENT_FIRST_MESSAGE, commentTypeButtons);
   },
-  [MAIN_BUTTON_TEXT.CATALOG]: (ctx) => {
+  [MAIN_BUTTON_TEXT.CATALOG]: ctx => {
     ctx.reply("voordon da! 2");
   },
-  [MAIN_BUTTON_TEXT.FAVS]: (ctx) => {
-    ctx.reply("voordon da! 3");
+  [MAIN_BUTTON_TEXT.FAVS]: async ctx => {
+    const user = await User.findOne({ telId: ctx.message.from.id }).populate(
+      "fav"
+    );
+    if (user) ctx.reply(FAV_LIST_MESSAGE, productListButton(user.fav));
+    else ctx.reply(FAV_LIST_EMPTY_MESSAGE);
   },
-  [MAIN_BUTTON_TEXT.CART]: (ctx) => {
+  [MAIN_BUTTON_TEXT.CART]: ctx => {
     ctx.reply("voordon da! 4");
   },
-  [MAIN_BUTTON_TEXT.MY_BUYS]: (ctx) => {
+  [MAIN_BUTTON_TEXT.MY_BUYS]: ctx => {
     ctx.reply("voordon da! 5");
-  },
+  }
 };
 
 module.exports.keyboardEventListener = EventListener;
--- app/bot/utils/MessageHandler.js
+++ app/bot/utils/MessageHandler.js
@@ -1,25 +1,35 @@
 module.exports.START_MESSAGE = "سلام به فروشگاه ما خوش آمدید";
 module.exports.CATEGORY_LIST = "لطفا یک گروه را برای ادامه انخاب کنید";
-module.exports.PRODUCT_LIST_MESSAGE = "یکی از محصولات زیر را برای دریافت اطلاعات بیشتر انتخاب کنید";
+module.exports.PRODUCT_LIST_MESSAGE =
+  "یکی از محصولات زیر را برای دریافت اطلاعات بیشتر انتخاب کنید";
 module.exports.PRODUCT_NOT_FOUND_MESSAGE = "محصول مورد نظر یافت نشد";
 module.exports.SEARCH_MESSAGE = "نام محصولی که میخواهید را تایپ کنید";
 module.exports.COMMENT_FIRST_MESSAGE = "لطفا نوع نظر خود را انتخاب کنید";
 module.exports.COMMENT_SECOND_MESSAGE = "هرچه میخواهد دل تنگت بوگو";
-module.exports.COMMENT_THIRD_MESSAGE = "با تشکر ، نظرتان برای بررسی بیشتر به ادمین ارسال شد";
-module.exports.FAV_ADDED_MESSAGE = "محصول فوق به لیست علاقه‌مندی شما افزوده شد"
-module.exports.getProductDetailMessage = (product) => `🌸${product.name}🌸
+module.exports.COMMENT_THIRD_MESSAGE =
+  "با تشکر ، نظرتان برای بررسی بیشتر به ادمین ارسال شد";
+module.exports.FAV_ADDED_MESSAGE = "محصول فوق به لیست علاقه‌مندی شما افزوده شد";
+module.exports.FAV_LIST_MESSAGE =
+  " محصولات زیر در لیست علاقه مندیهای شما قرار دارند. در صورت تمایل برای اطلاعات بیشتر و خرید آنها روی هر گزینه کلیک کنید.";
+module.exports.FAV_LIST_EMPTY_MESSAGE = "لیست علاقه مندی شما خالیه";
 
-${product.meta ? product.meta.map(item => (`${item.key}:${item.value}`)).join("\n") : ""}
+module.exports.getProductDetailMessage = product => `🌸${product.name}🌸
+
+${
+  product.meta
+    ? product.meta.map(item => `${item.key}:${item.value}`).join("\n")
+    : ""
+}
 
 وضعیت : ${product.exist ? "موجود" : "ناموجود"}
 قیمت مقطوع : ${product.price} تومان
 
-`
-module.exports.adminCommentMessage = (comment, user) => `${comment.type === "COMMENT_TYPE_CRIT" ? "یک انتقاد جدید" : "یک پیشنهاد جدید"}
+`;
+module.exports.adminCommentMessage = (comment, user) => `${
+  comment.type === "COMMENT_TYPE_CRIT" ? "یک انتقاد جدید" : "یک پیشنهاد جدید"
+}
 
 ${comment.text}
 
 کاربر : @${user.username}
-`
-
-
+`;
